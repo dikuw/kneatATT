0b1fbc3620ca6adc0595fd5d27770818f20cf8a0
Michael Vasilevsky <michael@dikuw.com>
2022-05-08 12:44:48 -0400

    add gotoWorkspace

--- server/shared/shared.js
+++ server/shared/shared.js
@@ -5,6 +5,15 @@ exports.login = async (page, username, password) => {
   await page.click('#rc-form-submit');
 }
 
+exports.gotoWorkspace = async (page, workspace) => {
+  await page.waitForSelector('[data-automationid="activeWorkspace"]');
+  await page.waitForTimeout(1000);
+  await page.click('[data-automationid="activeWorkspace"]');
+  await page.waitForTimeout(1000);
+  let [el] = await page.$x(`//span[contains(text(),"${workspace}")]`)
+  await el.click();
+}
+
 exports.gotoDocumentTemplates = async (page) => {
   await page.waitForSelector('.icon-doc-templates');
   await page.click('.icon-doc-templates');
